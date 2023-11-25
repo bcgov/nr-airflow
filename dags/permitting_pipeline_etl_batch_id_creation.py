b534c21fe4be3a06c76923d4a08054633b5facfc
vishreddy01 <126716045+vishreddy01@users.noreply.github.com>
2023-11-24 16:16:44 -0800

    Update permitting_pipeline_etl_batch_id_creation.py

--- dags/permitting_pipeline_etl_batch_id_creation.py
+++ dags/permitting_pipeline_etl_batch_id_creation.py
@@ -8,7 +8,7 @@ from datetime import datetime
 # Define default_args dictionary
 default_args = {
     'owner': 'airflow',
-    'start_date': datetime(2023, 8, 23),
+    'start_date': datetime(2023, 11, 23),
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
