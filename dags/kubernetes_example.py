33208ec9e44f5d7ed3ecd51e4e638e216fa82811
abimichel <abigail.michel@gov.bc.ca>
2023-11-28 14:12:36 -0800

    modified:   dags/kubernetes_example.py

--- dags/kubernetes_example.py
+++ dags/kubernetes_example.py
@@ -5,7 +5,6 @@ from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperato
 from airflow.providers.cncf.kubernetes.secret import Secret
 
 ods_secrets = Secret("env", None, "ods-database")
-print(ods_secrets)
 
 with DAG(
     start_date=datetime(2023, 11, 23),
@@ -15,13 +14,13 @@ with DAG(
 ) as dag:
     run_ats_replication = KubernetesPodOperator(
         task_id="run_container",
-        image="image-registry.openshift-image-registry.svc:5000/a1b9b0-dev/github-ping@sha256:305ee6ebbd43b6f576bbcc7fd3855d712b46e76668d1e422f350c4e7a7c9c620",
+        image="image-registry.openshift-image-registry.svc:5000/a1b9b0-dev/python@sha256:b94af75d4ff65c50bf1b2119bca6d0ba707037bacd0cb75314801a6953c03241",
         in_cluster=True,
         namespace="a1b9b0-dev",
         service_account_name="airflow-admin",
         name="run_container",
         random_name_suffix=True,
-        labels={"DataClass": "Medium", "env": "dev"},
+        labels={"DataClass": "Low", "env": "dev"},
         reattach_on_restart=True,
         is_delete_operator_pod=False,
         get_logs=True,
@@ -29,5 +28,6 @@ with DAG(
         secrets=[ods_secrets],
         container_resources= client.V1ResourceRequirements(
         requests={"cpu": "50m", "memory": "256Mi"},
-        limits={"cpu": "1", "memory": "1Gi"})
+        limits={"cpu": "1", "memory": "1Gi"}), 
+        cmds=["python3", "print(ods_secrets)"]
     )
