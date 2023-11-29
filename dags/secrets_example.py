726214285b8704cec3f589243badff6e4341deeb
abimichel <abigail.michel@gov.bc.ca>
2023-11-29 13:01:55 -0800

    modified:   dags/secrets_example.py

--- dags/secrets_example.py
+++ dags/secrets_example.py
@@ -4,14 +4,13 @@ from airflow.operators.python import PythonOperator
 from airflow.providers.cncf.kubernetes.secret import Secret
 import os
 
-ods_secrets = Secret(deploy_target="env", deploy_type = "env", secret = "ods-database", key="ODS_DATABASE")
+ods_secrets = Secret(deploy_target="env", deploy_type = "env", secret = "ods-database", key = "ODS_DATABASE")
+dict_secrets = ods_secrets.__dict__ 
 # ods_secrets.to_env_secret()
-# dict_secrets = ods_secrets.__dict__ 
-
 def print_secrets():
     print("secret object:", ods_secrets)
     print("env var database name:", os.getenv('ODS_DATABASE'))
-    # print("index secret:", ods_secrets.secret('ODS_DATABASE'))
+    print("dictionary secret:", dict_secrets)
 
 dag = DAG(
     dag_id='secrets_example',
@@ -23,8 +22,8 @@ dag = DAG(
 print_task = PythonOperator(
     task_id='secrets_example',
     python_callable=print_secrets,
-    provide_context=True,  
-    op_args=[ods_secrets],  
+    # provide_context=True,  
+    # op_args=[ods_secrets],  
     # env_vars={'ODS_DATABASE': '{{ var.value.ODS_DATABASE }}'},
     dag=dag
 )
