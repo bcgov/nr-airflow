6c29671dfe0061cdf1ca7d479aa3aff1a616b2b6
Vikas Grover <Vikas.Grover@gov.bc.ca>
2023-11-30 14:58:12 -0800

    Adding dags for rrs and fta

---
+++ dags/data_pipeline_dag_rrs.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+from data_replication_cls import data_replication
+
+dag = DAG('Permitting_Pipeline_RRS', description='Data Pipeline to execute replication and ETL for RRS tables', schedule_interval='0 12 * * *', start_date=datetime(2017, 3, 20), catchup=False)
+
+with dag:
+ dummy_task = DummyOperator(task_id='ETL_Start', retries = 3),
+
+ start_replication_rrs = data_replication(mstr_schema = 'app_rrs1', 
+                                          app_name = 'rrs', 
+                                          env = 'dev',
+                                          task_id='start_replication_rrs'
+                                          )
+
+dummy_task >> start_replication_rrs
