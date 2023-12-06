002f0fdff3a5f20fd976610096e602d99714d3db
abimichel <abigail.michel@gov.bc.ca>
2023-12-06 11:14:59 -0800

    rename (duplicate with other DAGs)

--- dags/data_pipeline_dag_fta.py
+++ dags/data_pipeline_dag_fta.py
@@ -4,7 +4,7 @@ from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 from data_replication_cls import data_replication
 
-dag = DAG('Permitting_Pipeline_FTA', description='Data Pipeline to execute replication and ETL for FTA tables', schedule_interval='0 12 * * *', start_date=datetime(2017, 3, 20), catchup=False)
+dag = DAG('data_pipeline_dag_fta', description='Data Pipeline to execute replication and ETL for FTA tables', schedule_interval='0 12 * * *', start_date=datetime(2017, 3, 20), catchup=False)
 
 with dag:
  dummy_task = DummyOperator(task_id='ETL_Start', retries = 3),
