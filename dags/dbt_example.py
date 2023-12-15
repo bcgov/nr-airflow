145e3e8e72237de951563892689ce7fed1152748
abimichel <abigail.michel@gov.bc.ca>
2023-12-15 14:15:52 -0800

    new file:   dags/dbt_example.py

---
+++ dags/dbt_example.py
@@ -0,0 +1,30 @@
+from airflow import DAG
+from pendulum import datetime
+from kubernetes import client
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+
+with DAG(
+    start_date=datetime(2023, 11, 23),
+    catchup=False,
+    schedule=None,
+    dag_id="dbt_example",
+) as dag:
+    run_ats_replication = KubernetesPodOperator(
+        task_id="init_dbt_container",
+        image="ghcr.io/dbt-labs/dbt-postgres:1.7.2",
+        in_cluster=True,
+        namespace="a1b9b0-dev",
+        service_account_name="airflow-admin",
+        name="run_dbt_container",
+        random_name_suffix=True,
+        labels={"DataClass": "Low", "env": "dev"},
+        reattach_on_restart=True,
+        is_delete_operator_pod=False,
+        get_logs=True,
+        log_events_on_failure=True,
+        container_resources= client.V1ResourceRequirements(
+        requests={"cpu": "50m", "memory": "256Mi"},
+        limits={"cpu": "1", "memory": "1Gi"}), 
+        cmds=["dbt init"]
+    )
+
