0ab2b5f02f3a3ed7fc66d9905b86f88265340a29
vishreddy01 <vish.reddy@gov.bc.ca>
2023-12-19 22:55:51 -0800

    rrs snapshot change

--- dags/dbt_rrs_snapshot.py
+++ dags/dbt_rrs_snapshot.py
@@ -12,7 +12,7 @@ with DAG(
 ) as dag:
     run_rrs_snapshot = KubernetesPodOperator(
         task_id="run_rrs_snapshot",
-        image="image-registry.openshift-image-registry.svc:5000/a1b9b0-dev/dbt-container-snapshot@sha256:ac3fd1ff5865adbfb293df850f1ee233557187fc7bf37717671024f7cdfe8019",
+        image="image-registry.openshift-image-registry.svc:5000/a1b9b0-dev/dbt-container-snapshot@sha256:19062274fffe9840ca695f455964709ea1a4cca23e688d7ea14ffe3a0a36aa87",
         in_cluster=True,
         namespace="a1b9b0-dev",
         service_account_name="airflow-admin",
