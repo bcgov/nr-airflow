0906b28556116577b8ce9279db09ec2a94591794
abimichel <abigail.michel@gov.bc.ca>
2023-12-29 14:38:48 -0800

    modified:   dags/airbyte_trigger_job.py

--- dags/airbyte_trigger_job.py
+++ dags/airbyte_trigger_job.py
@@ -4,10 +4,11 @@ import pendulum
 import json
 
 # API is located at http://dev-release-ab-airbyte-api-server-svc 
+# Job is called "Airbyte Tests as JSON"
 # job trigger sometimes does not working due to competing resources when running airflow/airbyte at the same time
 
 airbyte_job_type = "sync"
-airbyte_connection_id = "2b9f5022-51d3-4fdf-ba83-82d1b5d80ce8"
+airbyte_connection_id = "92c71303-a1f8-4ab6-a6be-28a47f000319"
 airbyte_workspace_id = "ea2db1ec-0357-4868-b216-4ca7333f5df4"
 
 with DAG(dag_id='airbyte_trigger_job',
