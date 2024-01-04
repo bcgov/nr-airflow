36d9d236921403c3eb77b45531d161147c39355f
vishreddy01 <vish.reddy@gov.bc.ca>
2024-01-03 17:28:47 -0800

    dbt rrs snapshot dag changes

--- dags/dbt_rrs_snapshot.py
+++ dags/dbt_rrs_snapshot.py
@@ -13,9 +13,10 @@ with DAG(
     run_rrs_snapshot = KubernetesPodOperator(
         task_id="run_rrs_snapshot",
         # Abi: need to push image to TEST namespace or pull via GHCR
-        image="image-registry.openshift-image-registry.svc:5000/a1b9b0-dev/dbt-container-snapshot@sha256:c1a7a01e2908cc1b12ec47923cbe12ed4b46a99b2ac15ff43955e53e152d8498",
+        #image="image-registry.openshift-image-registry.svc:5000/a1b9b0-test/dbt-container-snapshot@sha256:c1a7a01e2908cc1b12ec47923cbe12ed4b46a99b2ac15ff43955e53e152d8498",
+        image="image-registry.openshift-image-registry.svc:5000/a1b9b0-dev/dbt-container-snapshot-test@sha256:34d83df0f9ff15be82a981dfaf5fea878007378f6f92fe01731e2d8305da0b2b",
         in_cluster=True,
-        namespace="a1b9b0-dev",
+        namespace="a1b9b0-test",
         service_account_name="airflow-admin",
         name="run_rrs_snapshot",
         random_name_suffix=True,
@@ -26,5 +27,7 @@ with DAG(
         log_events_on_failure=True,
         container_resources= client.V1ResourceRequirements(
         requests={"cpu": "10m", "memory": "256Mi"},
-        limits={"cpu": "50m", "memory": "500Mi"})
+        limits={"cpu": "50m", "memory": "500Mi"},
+        cmds=["dbt"], 
+        arguments=["snapshot", "--profiles-dir", "/usr/app/dbt/.dbt"])
     )
