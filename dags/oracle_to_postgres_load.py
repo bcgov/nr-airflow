259f64df46364f4e8aeba6a8e75feb4ba995b8fe
vishreddy01 <vish.reddy@gov.bc.ca>
2024-02-06 11:56:42 -0800

    new change

--- dags/oracle_to_postgres_load.py
+++ dags/oracle_to_postgres_load.py
@@ -9,7 +9,7 @@ from airflow.providers.postgres.hooks.postgres import PostgresHook
 @task
 def get_data_from_oracle():
     oracle_hook = OracleHook(oracle_conn_id='oracle_fta_conn')
-    data = oracle_hook.get_pandas_df(sql="SELECT tenure_application_state_code,description FROM the.tenure_application_state_code")
+    data = oracle_hook.get_pandas_df(sql="SELECT TENURE_APPLICATION_STATE_CODE, DESCRIPTION,CAST( EFFECTIVE_DATE AS 'YYYY-MM-DD'),CAST( EXPIRY_DATE AS 'YYYY-MM-DD'),CAST( EXPIRY_DATE AS 'YYYY-MM-DD'),CAST(UPDATE_TIMESTAMP AS 'YYYY-MM_DD') FROM the.tenure_application_state_code")
     return data.to_dict()
 
 @task
