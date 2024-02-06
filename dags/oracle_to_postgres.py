f3efb50135e6fff029d48893d5ff6899c15255ec
vishreddy01 <vish.reddy@gov.bc.ca>
2024-02-05 16:28:16 -0800

    dag change

--- dags/oracle_to_postgres.py
+++ dags/oracle_to_postgres.py
@@ -7,7 +7,7 @@ from airflow.providers.postgres.hooks.postgres import PostgresHook
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2024, 1, 1),
+    'start_date': datetime(2024, 02, 05),
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
