025d3c555a8451478be5679863e8b51c665e6b39
vishreddy01 <vish.reddy@gov.bc.ca>
2024-02-12 15:20:18 -0800

    created the new dag

---
+++ dags/vault_remove_pmt_airflow_variables.py
@@ -0,0 +1,24 @@
+from airflow import DAG
+from airflow.models import Variable
+from airflow.operators.python_operator import PythonOperator
+from pendulum import datetime
+
+def remove_pmt_variables():
+    all_variables = Variable.get_variable('all', deserialize_json=True)
+    for key in all_variables.keys():
+        if key.startswith('pmt_'):
+            Variable.delete(key)
+
+with DAG(
+    start_date=datetime(2023, 11, 23),
+    catchup=False,
+    schedule_interval='@daily',
+    dag_id="vault_remove_pmt_airflow_variables",
+) as dag:
+
+    remove_pmt_variables_task = PythonOperator(
+        task_id="remove_pmt_variables_task",
+        python_callable=remove_pmt_variables
+    )
+
+remove_pmt_variables_task
