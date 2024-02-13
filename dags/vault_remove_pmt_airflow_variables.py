2cbfadd1c60d0b9087714f52b167c9f37aa54d61
vishreddy01 <vish.reddy@gov.bc.ca>
2024-02-12 17:55:20 -0800

    change in dag

--- dags/vault_remove_pmt_airflow_variables.py
+++ dags/vault_remove_pmt_airflow_variables.py
@@ -4,7 +4,7 @@ from airflow.operators.python_operator import PythonOperator
 from pendulum import datetime
 
 def remove_pmt_variables():
-    variable_list = list(Variable.get('pmt_all_creds'))
+    variable_list = Variable.get('pmt_all_creds')
     print(type(variable_list))
     print(variable_list)
     #for v in variable_list:
