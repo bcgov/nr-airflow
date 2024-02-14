7cc74f0ee48bcd390b9fc0ed0880145540000fe5
vishreddy01 <vish.reddy@gov.bc.ca>
2024-02-14 10:16:49 -0800

    chang in dag vars

--- dags/dbt_dlh_pmt_dim_org.py
+++ dags/dbt_dlh_pmt_dim_org.py
@@ -8,8 +8,8 @@ from airflow.models import Variable
 #ods_password = Variable.get("ods_password",deserialize_json=True)
 #dlh_password = Variable.get("dlh_password",deserialize_json=True)
 
-ods_password = Variable.get("ods_password",deserialize_json=True)
-dlh_password = Variable.get("dlh_password",deserialize_json=True)
+ods_password = Variable.get("ods_password")
+dlh_password = Variable.get("dlh_password")
 
 with DAG(
     start_date=datetime(2024, 2, 13),
