7ea93cf79dd4f26abdfdfb97a5c85fdc02359891
abimichel <136123575+abimichel@users.noreply.github.com>
2024-02-23 11:26:09 -0800

    turn DAG into template for PMT team

--- dags/demo_pmt_pipeline.py
+++ dags/demo_pmt_pipeline.py
@@ -5,29 +5,29 @@ from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperato
 from airflow.providers.cncf.kubernetes.secret import Secret
 
 ods_secrets = Secret("env", None, "ods-database")
-lexis_secrets = Secret("env", None, "lexis-database")
+LOB_secrets = Secret("env", None, "LOB-database") # Replace "LOB" with source system name (e.g. 'lexis')
 
 with DAG(
     start_date=datetime(2023, 11, 23),
     catchup=False,
     schedule=None,
-    dag_id="permitting_pipeline_lexis",
+    dag_id="permitting_pipeline_LOB", # Replace "LOB" with source system name (e.g. 'lexis')
 ) as dag:
-    run_lexis_replication = KubernetesPodOperator(
-        task_id="run_lexis_replication",
+    run_LOB_replication = KubernetesPodOperator( # Replace "LOB" with source system name (e.g. 'lexis')
+        task_id="run_LOB_replication", # Replace "LOB" with source system name (e.g. 'lexis')
         image="ghcr.io/bcgov/nr-permitting-pipelines:main",
         image_pull_policy="Always",
         in_cluster=True,
         namespace="a1b9b0-dev",
         service_account_name="airflow-admin",
-        name="run_lexis_replication",
+        name="run_LOB_replication", # Replace "LOB" with source system name (e.g. 'lexis')
         random_name_suffix=True,
         labels={"DataClass": "Medium", "ConnectionType": "database",  "Release": "test-release-af"}, 
         reattach_on_restart=True,
         is_delete_operator_pod=False,
         get_logs=True,
         log_events_on_failure=True,
-        secrets=[lexis_secrets, ods_secrets],
+        secrets=[LOB_secrets, ods_secrets], # Replace "LOB" with source system name (e.g. 'lexis')
         container_resources= client.V1ResourceRequirements(
         requests={"cpu": "50m", "memory": "512Mi"},
         limits={"cpu": "100m", "memory": "1024Mi"})
