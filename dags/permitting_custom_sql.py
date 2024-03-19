9de6b11d4439d9a8f24c9ec2e395b883a321bc99
abimichel <abigail.michel@gov.bc.ca>
2024-03-19 11:13:02 -0700

    X-NRS permitting test dag

--- dags/permitting_custom_sql.py
+++ dags/permitting_custom_sql.py
@@ -4,10 +4,10 @@ from kubernetes import client
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 from airflow.providers.cncf.kubernetes.secret import Secret
 
-LOB = '' # ats, fta, rrs, or lexis
+LOB = 'lexis' # ats, fta, rrs, or lexis
 
 ods_secrets = Secret("env", None, "ods-database")
-lob_secrets = Secret("env", None, f"{LOB}-database")
+lob_secrets = Secret("env", None, f"lexis-database")
 
 with DAG(
     start_date=datetime(2023, 11, 23),
@@ -16,20 +16,18 @@ with DAG(
     dag_id=f"permitting_pipeline_{LOB}",
 ) as dag:
     run_lexis_replication = KubernetesPodOperator(
-        task_id=f"run_{LOB}_replication",
+        task_id="run_replication",
         image="ghcr.io/bcgov/nr-permitting-pipelines:main",
         image_pull_policy="Always",
         in_cluster=True,
         service_account_name="airflow-admin",
-        name=f"run_{LOB}_replication",
-        random_name_suffix=True,
-        labels={"DataClass": "Medium", "ConnectionType": "database",  "Release": "test-release-af"}, 
-        reattach_on_restart=True,
+        name="run_replication",
+        labels={"DataClass": "Medium", "ConnectionType": "database",  "Release": "airflow"},
         is_delete_operator_pod=False,
-        get_logs=True,
-        log_events_on_failure=True,
         secrets=[lob_secrets, ods_secrets],
         container_resources= client.V1ResourceRequirements(
         requests={"cpu": "50m", "memory": "512Mi"},
         limits={"cpu": "100m", "memory": "1024Mi"})
     )
+
+KubernetesPodOperator()
