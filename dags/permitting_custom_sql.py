fbfa0ad87530fafbb81cfefbe351b8d555355410
abimichel <abigail.michel@gov.bc.ca>
2024-03-19 10:39:51 -0700

    clean up dag folder

--- dags/permitting_custom_sql.py
+++ dags/permitting_custom_sql.py
@@ -4,32 +4,32 @@ from kubernetes import client
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 from airflow.providers.cncf.kubernetes.secret import Secret
 
+LOB = '' # ats, fta, rrs, or lexis
+
 ods_secrets = Secret("env", None, "ods-database")
-ats_secrets = Secret("env", None, "ats-database")
+lob_secrets = Secret("env", None, f"{LOB}-database")
 
 with DAG(
     start_date=datetime(2023, 11, 23),
     catchup=False,
     schedule=None,
-    dag_id="permitting_pipeline_ats",
+    dag_id=f"permitting_pipeline_{LOB}",
 ) as dag:
-    run_ats_replication = KubernetesPodOperator(
-        task_id="run_ats_replication",
+    run_lexis_replication = KubernetesPodOperator(
+        task_id=f"run_{LOB}_replication",
         image="ghcr.io/bcgov/nr-permitting-pipelines:main",
         image_pull_policy="Always",
-        # image="image-registry.openshift-image-registry.svc:5000/a1b9b0-dev/data-replication-parametrized-audit1@sha256:8c51ee820434e4f5d06a91deda645bcd0a943b8c87bc3c8a8e67dead1c18a786",
         in_cluster=True,
-        namespace="a1b9b0-dev",
         service_account_name="airflow-admin",
-        name="run_ats_replication",
+        name=f"run_{LOB}_replication",
         random_name_suffix=True,
-        labels={"DataClass": "Medium", "ConnectionType": "database", "Release": "test-release-af"},  # network policies
+        labels={"DataClass": "Medium", "ConnectionType": "database",  "Release": "test-release-af"}, 
         reattach_on_restart=True,
         is_delete_operator_pod=False,
         get_logs=True,
         log_events_on_failure=True,
-        secrets=[ats_secrets, ods_secrets],
+        secrets=[lob_secrets, ods_secrets],
         container_resources= client.V1ResourceRequirements(
-        requests={"cpu": "10m", "memory": "256Mi"},
-        limits={"cpu": "50m", "memory": "500Mi"})
+        requests={"cpu": "50m", "memory": "512Mi"},
+        limits={"cpu": "100m", "memory": "1024Mi"})
     )
