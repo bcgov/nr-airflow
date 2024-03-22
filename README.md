f29a3f82f869e207c1247368ca10b54f78f13dcd
abimichel <abigail.michel@gov.bc.ca>
2024-03-22 11:24:25 -0700

    Merge branch 'vault' of https://github.com/bcgov/nr-airflow into vault

--- README.md
+++ README.md
@@ -6,24 +6,25 @@ helm pull oci://registry-1.docker.io/bitnamicharts/airflow
 ```
 
 ## Deploying to OpenShift
-```sh
-helm install test-release-af .
-```
-
-## Visit the application here:
-http://nr-airflow-dev.apps.emerald.devops.gov.bc.ca/
-
-## Upgrade OpenShift Deployment
-```sh 
-helm upgrade -f values.yaml test-release-af . --version 16.1.2  
-```
-
-## Create OpenShift ConfigMap for requirements.txt: 
+Create OpenShift ConfigMap for requirements.txt:
 ```sh
 oc create configmap airflow-requirements --from-file=requirements.txt
 ```
-Delete if already exists
+Navigate to the 'oc' folder then:
+```sh
+oc apply -f .
+```
+Navigate to the 'airflow' folder then:
+```sh
+helm install airflow .
+```
 
+## Visit the application here:
+http://nr-airflow.apps.emerald.devops.gov.bc.ca/
+
+## Upgrade OpenShift Deployment
+```sh 
+helm upgrade -f values.yaml airflow .
+```
 
 More info: https://apps.nrs.gov.bc.ca/int/confluence/x/zQ09Cg
-
---
+++ dags/permitting_lexis.py
@@ -0,0 +1,31 @@
+from airflow import DAG
+from pendulum import datetime
+from kubernetes import client
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+from airflow.providers.cncf.kubernetes.secret import Secret
+
+LOB = 'lexis' # ats, fta, rrs, or lexis
+
+ods_secrets = Secret("env", None, "ods-database")
+lob_secrets = Secret("env", None, f"{LOB}-database")
+
+with DAG(
+    start_date=datetime(2023, 11, 23),
+    catchup=False,
+    schedule='10 6 * * *',
+    dag_id=f"permitting-pipeline-{LOB}",
+) as dag:
+    run_lexis_replication = KubernetesPodOperator(
+        task_id="run_replication",
+        image="ghcr.io/bcgov/nr-permitting-pipelines:main",
+        image_pull_policy="Always",
+        in_cluster=True,
+        service_account_name="airflow-admin",
+        name=f"run_{LOB}replication",
+        labels={"DataClass": "Medium", "ConnectionType": "database",  "Release": "airflow"},
+        is_delete_operator_pod=False,
+        secrets=[lob_secrets, ods_secrets],
+        container_resources= client.V1ResourceRequirements(
+        requests={"cpu": "50m", "memory": "512Mi"},
+        limits={"cpu": "100m", "memory": "1024Mi"})
+    )
