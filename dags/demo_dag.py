f9c214ac147b79162b13050b7cc90632055ba76e
abimichel <abigail.michel@gov.bc.ca>
2024-03-22 12:00:09 -0700

    add comments to demo dag

--- dags/demo_dag.py
+++ dags/demo_dag.py
@@ -11,15 +11,16 @@ with DAG(
 ) as dag:
     python_container = KubernetesPodOperator(
         task_id="run_container",
-        image="artifacts.developer.gov.bc.ca/docker-remote/python",
-        image_pull_policy="IfNotPresent",
-        image_pull_secrets="artifactory-pull",
-        in_cluster=True,
-        service_account_name="airflow-admin",
-        name="run_container",
-        labels={"DataClass": "Low", "env": "prod"},
-        container_resources= client.V1ResourceRequirements(
-        requests={"cpu": "50m", "memory": "68Mi"},
-        limits={"cpu": "100m", "memory": "200Mi"}),
-        cmds=["python3"]
+        image="artifacts.developer.gov.bc.ca/docker-remote/python",       # Container image to be used
+        image_pull_policy="IfNotPresent",                                 # Policy for pulling the Docker image
+        image_pull_secrets="artifactory-pull",                            # Image pull secret (only required when using Artifactory)
+        service_account_name="airflow-admin",                             # Service account to be used for running the pod
+        name="run_container",                                             # Name of the pod                                     # Add a random suffix to the pod name
+        labels={"DataClass": "Low", "env": "dev"},                        # Labels to be assigned to the pod
+        log_events_on_failure=True,                                       # Log events in case of task failure
+        container_resources=client.V1ResourceRequirements(
+            requests={"cpu": "50m", "memory": "256Mi"},                   # Resource requests for the container
+            limits={"cpu": "1", "memory": "1Gi"},                         # Resource limits for the container
+        cmds=["python3"])                                                 # Command to be executed in the container
+
     )
