36c074e5243277d69a73456e10b184fcc5404d1d
vishreddy01 <vish.reddy@gov.bc.ca>
2024-04-08 15:11:24 -0700

    new test dag

---
+++ dags/demo_dag2.py
@@ -0,0 +1,25 @@
+from airflow import DAG
+from pendulum import datetime
+from kubernetes import client
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+
+with DAG(
+    start_date=datetime(2024, 3, 20),
+    catchup=False,
+    schedule=None,
+    dag_id="demo-dag2"
+) as dag:
+    python_container2 = KubernetesPodOperator(
+        task_id="run_container2",
+        image="artifacts.developer.gov.bc.ca/docker-remote/python",
+        image_pull_policy="IfNotPresent",
+        image_pull_secrets="artifactory-pull",
+        in_cluster=True,
+        service_account_name="airflow-admin",
+        name="run_container2",
+        labels={"DataClass": "Low", "env": "prod"},
+        container_resources= client.V1ResourceRequirements(
+        requests={"cpu": "50m", "memory": "68Mi"},
+        limits={"cpu": "100m", "memory": "200Mi"}),
+        cmds=["python3"]
+    )
