1ba2daf53b24bf5a0d37874cd166b9f5c31db98b
vishreddy01 <vish.reddy@gov.bc.ca>
2024-04-17 09:40:02 -0700

    Added a script to load data from pf 2 pg

---
+++ dags/demo_pg_to_pg_replication.py
@@ -0,0 +1,42 @@
+from airflow import DAG
+from pendulum import datetime
+from kubernetes import client
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+from airflow.providers.cncf.kubernetes.secret import Secret
+
+ods_secrets = Secret("env", None, "ods-database")
+dlh_secrets = Secret("env", None, "dlh-database")
+
+default_args = {
+    "email": ["NRM.DataFoundations@gov.bc.ca"],
+    "email_on_failure": True,
+    "email_on_retry": True,
+}
+
+with DAG(
+    start_date=datetime(2023, 11, 23),
+    catchup=False,
+    schedule=None,
+    dag_id="demo_pg_to_pg_replication",
+    default_args=default_args,
+) as dag:
+    run_ats_replication = KubernetesPodOperator(
+        task_id="run_pg_to_pg_replication",
+        image="ghcr.io/bcgov/nr-dap-ods-pg2pg:ods-replication-pg2pg",
+        image_pull_policy="Always",
+        # image="image-registry.openshift-image-registry.svc:5000/a1b9b0-dev/data-replication-parametrized-audit1@sha256:8c51ee820434e4f5d06a91deda645bcd0a943b8c87bc3c8a8e67dead1c18a786",
+        in_cluster=True,
+        namespace="a1b9b0-dev",
+        service_account_name="airflow-admin",
+        name="run_pg_to_pg_replication",
+        random_name_suffix=True,
+        labels={"DataClass": "Medium", "ConnectionType": "database", "Release": "test-release-af"},  # network policies
+        reattach_on_restart=True,
+        is_delete_operator_pod=False,
+        get_logs=True,
+        log_events_on_failure=True,
+        secrets=[ods_secrets, dlh_secrets],
+        container_resources= client.V1ResourceRequirements(
+        requests={"cpu": "10m", "memory": "256Mi"},
+        limits={"cpu": "50m", "memory": "500Mi"})
+    )
