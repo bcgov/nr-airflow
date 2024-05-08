6a9c3c978609914f6b91268212c4a7d2c43c996c
abimichel <136123575+abimichel@users.noreply.github.com>
2024-05-08 14:06:20 -0700

    add description to demo_dag.py

--- dags/demo_dag.py
+++ dags/demo_dag.py
@@ -15,10 +15,11 @@ with DAG(
     schedule=None,
     dag_id="demo-dag",
     default_args=default_args,
+    description='DAG to demo Kubernetes pod operator',
 ) as dag:
     python_container = KubernetesPodOperator(
         task_id="run_container",
-        image="artifacts.developer.gov.bc.ca/docker-remote/notarealimage",       # Container image to be used
+        image="artifacts.developer.gov.bc.ca/docker-remote/python",       # Container image to be used
         image_pull_policy="IfNotPresent",                                 # Policy for pulling the Docker image
         image_pull_secrets="artifactory-pull",                            # Image pull secret (only required when using Artifactory)
         service_account_name="airflow-admin",                             # Service account to be used for running the pod
