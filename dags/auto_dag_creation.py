8ad9bc8800f95d8f4fb8f845c7a3c3c57c1a1cdd
vishreddy01 <126716045+vishreddy01@users.noreply.github.com>
2024-05-14 17:58:35 -0700

    Update auto_dag_creation.py

--- dags/auto_dag_creation.py
+++ dags/auto_dag_creation.py
@@ -13,7 +13,7 @@ default_args = {
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    'retry_delay': timedelta(minutes=1),
 }
 
 def execute_python_script():
