065eaa5dc61e3c0ca68eefc105f544b135902d50
vishreddy01 <126716045+vishreddy01@users.noreply.github.com>
2024-05-14 17:58:20 -0700

    Update auto_dag_creation.py

--- dags/auto_dag_creation.py
+++ dags/auto_dag_creation.py
@@ -17,7 +17,7 @@ default_args = {
 }
 
 def execute_python_script():
-    
+
     # Retrieve PostgreSQL connection parameters from Airflow variables
     postgres_host = Variable.get('ODS_HOST')
     postgres_port = Variable.get('ODS_PORT')
@@ -45,14 +45,102 @@ def execute_python_script():
 
         return applications
 
+    # Function to create a DAG definition
+    def create_dag_definition(application):
+        dag_id = f"pipeline-{application}"
+        app = application
+        #LOB = application
+        DAG = f""" 
+    from airflow import DAG
+    from pendulum import datetime
+    from kubernetes import client
+    from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+    from airflow.providers.cncf.kubernetes.secret import Secret
+    from datetime import timedelta
+
+    LOB = "{app}"
+    LOB = LOB.lower()
+
+    default_args = {{
+        "email": ["NRM.DataFoundations@gov.bc.ca"],
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),
+        "email_on_failure": True,
+        "email_on_retry": True,
+    }}
+
+    with DAG(
+        start_date=datetime(2023, 11, 23),
+        catchup=False,
+        schedule='5 12 * * *',
+        dag_id=f"pipeline-{{LOB}}",
+        default_args=default_args,
+    ) as dag:
+        ods_secrets = Secret("env", None, "ods-database")
+        lob_secrets = Secret("env", None, f"{{LOB}}-database")
+
+        run_replication = KubernetesPodOperator(
+            task_id="run_replication",
+            image="ghcr.io/bcgov/nr-dap-ods:main",
+            image_pull_policy="Always",
+            in_cluster=True,
+            service_account_name="airflow-admin",
+            name=f"run_{{LOB}}_replication",
+            labels={{"DataClass": "Medium", "ConnectionType": "database",  "Release": "airflow"}},
+            is_delete_operator_pod=False,
+            secrets=[lob_secrets, ods_secrets],
+            container_resources=client.V1ResourceRequirements(
+                requests={{"cpu": "50m", "memory": "512Mi"}},
+                limits={{"cpu": "100m", "memory": "1024Mi"}}
+            )
+        )"""
+        DAG = str(DAG)
+
+        g = Github(github_secret)
+        repo = g.get_repo('bcgov/nr-airflow')
+
+        repo.create_file(f'dags/{dag_id}.py', 'upload dags', DAG, branch='ui-testing')
+        
+        repo = g.get_repo("bcgov/nr-airflow")
+        contents = repo.get_contents("/dags", ref="ui-testing")
+        print([c for c in contents])
+        for content_file in contents:
+            print(content_file)
+        return DAG
+
+
+    def extract_names(input_list):
+        output_list = []
+        prefix = "dags/pipeline-"
+        suffix = ".py"
+        for item in input_list:
+            # Remove the prefix and suffix
+            name = item[len(prefix):-len(suffix)]
+            output_list.append(name)
+        return output_list
+
     # Main function to orchestrate the process
     def main():
+        g = Github(github_secret)
+        repo = g.get_repo("bcgov/nr-airflow")
+        contents = repo.get_contents("/dags", ref="ui-testing")
+        existing_dags= [c.path for c in contents]
+        existing_dag_list = extract_names(existing_dags)
+        print(existing_dag_list)
+    
         new_applications = scan_table_for_new_apps()
-        print(new_applications)
+
+        for application in new_applications:
+            application = application[0]  # Extract application name
+            if application not in existing_dag_list:
+                dag_content = create_dag_definition(application)
+                print(dag_content)
+                existing_dag_list.append(application)
+                #push_to_github(dag_content, application)
+                #existing_dags.add(application)
 
     if __name__ == "__main__":
         main()
-    print(new_applications = scan_table_for_new_apps())
 
 with DAG(
     dag_id='auto_dag_creation',
