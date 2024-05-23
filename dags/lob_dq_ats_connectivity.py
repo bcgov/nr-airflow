0385e8105d29bec1e53a3342165a9718407437d9
J.A. <75276655+JTA2020@users.noreply.github.com>
2024-05-23 11:01:48 -0700

    Update lob_dq_ats_connectivity.py

--- dags/lob_dq_ats_connectivity.py
+++ dags/lob_dq_ats_connectivity.py
@@ -26,7 +26,7 @@ with DAG(
         image_pull_policy="Always",
         # image="image-registry.openshift-image-registry.svc:5000/a1b9b0-dev/data-replication-parametrized-audit1@sha256:8c51ee820434e4f5d06a91deda645bcd0a943b8c87bc3c8a8e67dead1c18a786",
         in_cluster=True,
-        namespace="a1b9b0-dev",
+        namespace="a1b9b0-test",
         service_account_name="airflow-admin",
         name="run_ats_connectivity_replication",
         random_name_suffix=True,
