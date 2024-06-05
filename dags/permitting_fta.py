f009ea14b6a8a4c5acfef75f2909001df7838d82
JTA2020 <athwaldata@gmail.com>
2024-06-05 16:36:29 -0700

    Update permitting_fta.py

--- dags/permitting_fta.py
+++ dags/permitting_fta.py
@@ -4,6 +4,7 @@ from pendulum import datetime
 from kubernetes import client
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 from airflow.providers.cncf.kubernetes.secret import Secret
+from datetime import timedelta
 
 LOB = 'fta' # ats, fta, rrs, or lexis
 
@@ -11,7 +12,10 @@ ods_secrets = Secret("env", None, "ods-database")
 lob_secrets = Secret("env", None, f"{LOB}-database")
 
 default_args = {
+    'owner': 'PMT',
     "email": ["NRM.DataFoundations@gov.bc.ca"],
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
     "email_on_failure": True,
     "email_on_retry": True,
 }
@@ -19,17 +23,18 @@ default_args = {
 with DAG(
     start_date=datetime(2023, 11, 23),
     catchup=False,
-    schedule='5 6 * * *',
+    schedule='5 12 * * *',
     dag_id=f"permitting-pipeline-{LOB}",
     default_args=default_args,
+    description='DAG to replicate FTA query to ODS for X-NRS Permitting Dashboard'
 ) as dag:
-    run_lexis_replication = KubernetesPodOperator(
+    run_replication = KubernetesPodOperator(
         task_id="run_replication",
         image="ghcr.io/bcgov/nr-dap-ods:main",
         image_pull_policy="Always",
         in_cluster=True,
         service_account_name="airflow-admin",
-        name=f"run_{LOB}replication",
+        name=f"run_{LOB}_replication",
         labels={"DataClass": "Medium", "ConnectionType": "database",  "Release": "airflow"},
         is_delete_operator_pod=False,
         secrets=[lob_secrets, ods_secrets],
