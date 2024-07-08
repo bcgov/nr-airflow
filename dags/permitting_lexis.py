c911beacd985cc9d8a8c553d2468dd5849f762ae
abimichel <abigail.michel@gov.bc.ca>
2024-07-08 14:17:48 -0700

    updating rbac for replication

--- dags/permitting_lexis.py
+++ dags/permitting_lexis.py
@@ -3,12 +3,18 @@ from pendulum import datetime
 from kubernetes import client
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 from airflow.providers.cncf.kubernetes.secret import Secret
+from datetime import timedelta
 
-ods_secrets = Secret("env", None, "ods-database")
-fta_secrets = Secret("env", None, "fta-database1")
+LOB = 'lexis' # ats, fta, rrs, or lexis
+
+ods_secrets = Secret("env", None, f"{LOB}-ods-database")
+lob_secrets = Secret("env", None, f"{LOB}-database")
 
 default_args = {
+    'owner': 'PMT',
     "email": ["NRM.DataFoundations@gov.bc.ca"],
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
     "email_on_failure": True,
     "email_on_retry": True,
 }
@@ -16,26 +22,21 @@ default_args = {
 with DAG(
     start_date=datetime(2023, 11, 23),
     catchup=False,
-    schedule=None,
-    dag_id="permitting_pipeline_fta",
+    schedule='10 12 * * *',
+    dag_id=f"permitting-pipeline-{LOB}",
     default_args=default_args,
+    description='DAG to replicate LEXIS data to ODS for X-NRS Permitting Dashboard'
 ) as dag:
-    run_fta_replication = KubernetesPodOperator(
-        task_id="run_fta_replication",
+    run_replication = KubernetesPodOperator(
+        task_id="run_replication",
         image="ghcr.io/bcgov/nr-dap-ods:main",
         image_pull_policy="Always",
-        # image="image-registry.openshift-image-registry.svc:5000/a1b9b0-dev/data-replication-parametrized-audit1@sha256:8c51ee820434e4f5d06a91deda645bcd0a943b8c87bc3c8a8e67dead1c18a786",
         in_cluster=True,
-        namespace="a1b9b0-dev",
         service_account_name="airflow-admin",
-        name="run_fta_replication",
-        random_name_suffix=True,
-        labels={"DataClass": "Medium", "ConnectionType": "database",  "Release": "test-release-af"},  # network policies
-        reattach_on_restart=True,
+        name=f"run_{LOB}_replication",
+        labels={"DataClass": "Medium", "ConnectionType": "database",  "Release": "airflow"},
         is_delete_operator_pod=False,
-        get_logs=True,
-        log_events_on_failure=True,
-        secrets=[fta_secrets, ods_secrets],
+        secrets=[lob_secrets, ods_secrets],
         container_resources= client.V1ResourceRequirements(
         requests={"cpu": "50m", "memory": "512Mi"},
         limits={"cpu": "100m", "memory": "1024Mi"})
