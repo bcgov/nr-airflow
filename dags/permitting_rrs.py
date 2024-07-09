8e2925acd1cd5905aeebe9f8cbb2c251fa31edb0
abimichel <abigail.michel@gov.bc.ca>
2024-07-09 14:33:15 -0700

    update branch with latest ats, fta, rrs, lexis DAGs

--- dags/permitting_rrs.py
+++ dags/permitting_rrs.py
@@ -1,4 +1,3 @@
-
 from airflow import DAG
 from pendulum import datetime
 from kubernetes import client
@@ -6,10 +5,10 @@ from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperato
 from airflow.providers.cncf.kubernetes.secret import Secret
 from datetime import timedelta
 
-LOB = 'fta'
+LOB = 'rrs' 
 
-ods_secrets = Secret("env", None, "ods-database")
-lob_secrets = Secret("env", None, f"{LOB}-replication-database")
+ods_secrets = Secret("env", None, f"{LOB}-ods-database")
+lob_secrets = Secret("env", None, f"{LOB}-database")
 
 default_args = {
     'owner': 'PMT',
@@ -23,10 +22,10 @@ default_args = {
 with DAG(
     start_date=datetime(2023, 11, 23),
     catchup=False,
-    schedule='25 12 * * *',
-    dag_id=f"permitting-pipeline-{LOB}-replication",
+    schedule='15 12 * * *',
+    dag_id=f"permitting-pipeline-{LOB}",
     default_args=default_args,
-    description='DAG to replicate FTA tables to ODS for Permitting Modernization Initiative'
+    description='DAG to replicate RRS data to ODS for X-NRS Permitting Dashboard'
 ) as dag:
     run_replication = KubernetesPodOperator(
         task_id="run_replication",
@@ -36,7 +35,7 @@ with DAG(
         service_account_name="airflow-admin",
         name=f"run_{LOB}_replication",
         labels={"DataClass": "Medium", "ConnectionType": "database",  "Release": "airflow"},
-        is_delete_operator_pod=False,
+        is_delete_operator_pod=True,
         secrets=[lob_secrets, ods_secrets],
         container_resources= client.V1ResourceRequirements(
         requests={"cpu": "50m", "memory": "512Mi"},
