eb1baa22267aebc88d7c69c8ea1c70fd3f8731ea
abimichel <abigail.michel@gov.bc.ca>
2024-07-09 16:27:01 -0700

    allow to run in any namespace

--- dags/lob_dq_ats_connectivity.py
+++ dags/lob_dq_ats_connectivity.py
@@ -26,13 +26,11 @@ with DAG(
         task_id="run_ats_connectivity_replication",
         image="ghcr.io/bcgov/nr-dap-ods:main",
         image_pull_policy="Always",
-        # image="image-registry.openshift-image-registry.svc:5000/a1b9b0-dev/data-replication-parametrized-audit1@sha256:8c51ee820434e4f5d06a91deda645bcd0a943b8c87bc3c8a8e67dead1c18a786",
         in_cluster=True,
-        namespace="a1b9b0-prod",
         service_account_name="airflow-admin",
         name="run_ats_connectivity_replication",
         random_name_suffix=True,
-        labels={"DataClass": "Medium", "ConnectionType": "database", "Release": "test-release-af"},  # network policies
+        labels={"DataClass": "Medium", "ConnectionType": "database", "Release": "airflow"},  # network policies
         reattach_on_restart=True,
         is_delete_operator_pod=False,
         get_logs=True,
