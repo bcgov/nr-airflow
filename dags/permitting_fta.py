0b828845b29258f3d6e0114eb633921005064317
abimichel <136123575+abimichel@users.noreply.github.com>
2024-07-16 15:42:30 -0700

    Modify resource request permitting_fta.py

--- dags/permitting_fta.py
+++ dags/permitting_fta.py
@@ -39,5 +39,5 @@ with DAG(
         secrets=[lob_secrets, ods_secrets],
         container_resources= client.V1ResourceRequirements(
         requests={"cpu": "50m", "memory": "512Mi"},
-        limits={"cpu": "100m", "memory": "2Gi"})
+        limits={"cpu": "100m", "memory": "1500Mi"})
     )
