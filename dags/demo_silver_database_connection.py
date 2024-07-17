aa7e919850ecfaa2545597560c84f621f90196ca
abimichel <136123575+abimichel@users.noreply.github.com>
2024-07-16 17:39:42 -0700

    Update demo_silver_database_connection.py

--- dags/demo_silver_database_connection.py
+++ dags/demo_silver_database_connection.py
@@ -7,7 +7,7 @@ from airflow.utils.dates import datetime
     "demo_silver_database_connection",
     start_date=datetime(2024, 1, 16),
     max_active_runs=3,
-    schedule="None",
+    schedule=None,
     template_searchpath="/opt/bitnami/airflow/dags/git_nr-airflow-dags/include/",
     catchup=False
 )
