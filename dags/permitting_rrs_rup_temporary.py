3ec1bab517e16b0c9b3d09e88a77e39f94e31382
abimichel <abigail.michel@gov.bc.ca>
2024-09-26 14:46:23 -0700

    update with latest permitting dags

--- dags/permitting_rrs_rup_temporary.py
+++ dags/permitting_rrs_rup_temporary.py
@@ -1,3 +1,5 @@
+# IMPORTANT: Delete this DAG once X-NRS Dashboard is being refreshed from like-to-like replicated tables
+
 from airflow import DAG
 from pendulum import datetime
 from kubernetes import client
@@ -5,10 +7,10 @@ from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperato
 from airflow.providers.cncf.kubernetes.secret import Secret
 from datetime import timedelta
 
-LOB = 'fta'
+LOB = 'rrs-rup'
 
-ods_secrets = Secret("env", None, f"{LOB}-ods-database")
-lob_secrets = Secret("env", None, f"{LOB}-database")
+ods_secrets = Secret("env", None, "ods-database")
+lob_secrets = Secret("env", None, f"{LOB}-database-temporary")
 
 default_args = {
     'owner': 'PMT',
@@ -22,10 +24,10 @@ default_args = {
 with DAG(
     start_date=datetime(2023, 11, 23),
     catchup=False,
-    schedule='5 12 * * *',
-    dag_id=f"permitting-pipeline-{LOB}",
+    schedule='20 12 * * *',
+    dag_id=f"permitting-pipeline-{LOB}-temporary",
     default_args=default_args,
-    description='DAG to replicate FTA data to ODS for X-NRS Permitting Dashboard'
+    description='DAG to replicate RRS query to ODS for X-NRS Permitting Dashboard'
 ) as dag:
     run_replication = KubernetesPodOperator(
         task_id="run_replication",
@@ -35,9 +37,9 @@ with DAG(
         service_account_name="airflow-admin",
         name=f"run_{LOB}_replication",
         labels={"DataClass": "Medium", "ConnectionType": "database",  "Release": "airflow"},
-        is_delete_operator_pod=True,
+        is_delete_operator_pod=False,
         secrets=[lob_secrets, ods_secrets],
         container_resources= client.V1ResourceRequirements(
         requests={"cpu": "50m", "memory": "512Mi"},
-        limits={"cpu": "100m", "memory": "4500Mi"})
+        limits={"cpu": "100m", "memory": "1024Mi"})
     )
