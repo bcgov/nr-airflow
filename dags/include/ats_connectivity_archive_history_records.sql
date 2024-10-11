3395e4391dc093bcc48019e8cfd05a1c9c64d9ae
Jakia Sultana <jakia.sultana2318@yahoo.com>
2024-10-11 11:52:54 -0700

    Modified insertion script for new column

--- dags/include/ats_connectivity_archive_history_records.sql
+++ dags/include/ats_connectivity_archive_history_records.sql
@@ -1,3 +1,28 @@
 --Step 4: Archive the Cur table snapshot
-INSERT INTO lob_dq_replication_hist.ats_connectivity_hist_arch
-Select a.*,current_timestamp(0) from lob_dq_replication_hist.ats_connectivity_hist a
+INSERT INTO lob_dq_replication_hist.ats_connectivity_hist_arch 
+select 
+    a.ministry_code,
+    a.business_area,
+    a.permit_type,
+    a.project_id,
+    a.application_id,
+    a.project_name,
+    a.project_description,
+    a.project_location,
+    a.received_date,
+    a.adjudication_date,
+    a.region_name,
+    a.estimated_houses_connected,
+    a.application_status,
+    a.business_area_file_number,
+    a.hash_key,
+    a.record_active_ind,
+    a.effective_start_dttm,
+    a.effective_end_dttm,
+    a.record_created_by,
+    a.record_created_dttm,
+    current_timestamp(0) archive_dttm,
+    a.close_code,
+    a.status_code,
+    a.close_reason
+  FROM	lob_dq_replication_hist.ats_connectivity_hist  a
