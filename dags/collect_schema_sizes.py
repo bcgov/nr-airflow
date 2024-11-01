c7cf74fa493a8cc69fa2aab1fd82f48201139819
IDIR\RPARDOFI <richard.pardo-figueroa@gov.bc.ca>
2024-11-01 16:40:32 -0700

    -- Simplified version, aligned with Vault secrets

--- dags/collect_schema_sizes.py
+++ dags/collect_schema_sizes.py
@@ -16,67 +16,22 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-def collect_schema_sizes():
-    # Set up logging
-    logger = logging.getLogger("airflow.task")
-
-    # Retrieve PostgreSQL connection parameters from Airflow variables
-    postgres_host = Variable.get('ODS_HOST')
-    postgres_port = Variable.get('ODS_PORT')
-    postgres_dbname = Variable.get('ODS_DATABASE')
-    postgres_user = Variable.get('ODS_USERNAME')
-    postgres_password = Variable.get('ODS_PASSWORD')
-
-    logger.info("Connecting to PostgreSQL database")
-
-    try:
-        # Connect to PostgreSQL
-        conn = psycopg2.connect(
-            host=postgres_host,
-            port=postgres_port,
-            dbname=postgres_dbname,
-            user=postgres_user,
-            password=postgres_password
-        )
-        cursor = conn.cursor()
-
-        logger.info("Executing collect_schema_sizes function")
-
-        # Execute the collect_schema_sizes function
-        cursor.execute("SELECT ods_data_management.collect_schema_sizes();")
-        conn.commit()
-
-        logger.info("Schema sizes collected successfully")
-
-    except Exception as e:
-        logger.error(f"Error collecting schema sizes: {e}")
-        raise
-
-    finally:
-        # Close the connection
-        cursor.close()
-        conn.close()
-        logger.info("PostgreSQL connection closed")
-
 with DAG(
     dag_id='collect_schema_sizes_dag',
+    start_date=datetime(2024, 11, 1),
+    max_active_runs=3,
+    schedule_interval='0 8 1 * *',  # At 08:00 on Fist day of the month   
     default_args=default_args,
-    description='DAG to collect PostgreSQL schema sizes monthly',
-
-    schedule_interval='0 8 1 * *',  # At 08:00 on day-of-month 1, 2am pacific time
-   # schedule_interval='0 8 * * *',  # Every day at 08:00 system time, 2am  pacific time
-    start_date=datetime(2023, 10, 1),
+    template_searchpath="/opt/bitnami/airflow/dags/git_nr-airflow-dags/include/",
     catchup=False,
-    tags=['schema-sizes'],
+    description='DAG to collect PostgreSQL schema sizes monthly'
 ) as dag:
 
-    start_task = EmptyOperator(
-        task_id='start_task',
-    )
+  gather_monthly_sizes = PostgresOperator(
+       task_id='execute_schema_size_function',
+       sql="'SELECT ods_data_management.collect_schema_sizes();'",
+       postgres_conn_id="postgres_ods_conn",
+       autocommit=True,
+   )
 
-    collect_schema_sizes_task = PythonOperator(
-        task_id='collect_schema_sizes_task',
-        python_callable=collect_schema_sizes,
-    )
-
-    start_task >> collect_schema_sizes_task
+  gather_monthly_sizes 
