ddf190d02a630183779f394042ebd602ff861c1b
IDIR\RPARDOFI <richard.pardo-figueroa@gov.bc.ca>
2024-11-07 10:40:51 -0800

    Change code to make it more aligned with standards within DAP

--- dags/ods_collect_schema_sizes.py
+++ dags/ods_collect_schema_sizes.py
@@ -1,36 +1,34 @@
-from airflow import DAG
-from airflow.models import Variable
-from airflow.operators.python_operator import PythonOperator
-from airflow.operators.postgres_operator import PostgresOperator
-from airflow.operators.empty import EmptyOperator
+from airflow.decorators import dag
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 from datetime import datetime, timedelta
-import logging
+from airflow.operators.postgres_operator import PostgresOperator
+from airflow.utils.dates import datetime
+from airflow.operators.python_operator import PythonOperator
 
 default_args = {
     'owner': 'Data Foundations',
-    'depends_on_past': False,
-    'email': ['NRM.DataFoundations@gov.bc.ca'],
-    'email_on_failure': True,
-    'email_on_retry': True,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    "depends_on_past": False,
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=1),
+    "email": ["NRM.DataFoundations@gov.bc.ca"],
+    "email_on_failure": True,
+    "email_on_retry": True,
 }
 
-with DAG(
-    dag_id='ods_collect_schema_stats_dag',
+@dag(
+    "ods_collect_schema_stats_dag",
     start_date=datetime(2024, 11, 10),
     max_active_runs=3,
-    schedule_interval='0 8 1 * *',  # At 08:00 on Fist day of the month   
+    schedule_interval='0 8 1 * *',  # At 08:00 on Fist day of the month  
     default_args=default_args,
     template_searchpath="/opt/bitnami/airflow/dags/git_nr-airflow-dags/include/",
     catchup=False,
-    description='DAG to collect PostgreSQL schema stats monthly'
-) as dag:
+    description='DAG to collect PostgreSQL schema stats monthly',
+)
+def exe_ods_collect_schema_stats():
 
-  logger = logging.getLogger("airflow.task")
-  logger.info("Connecting to PostgreSQL database")
-  
-  logger.info("Executing collect_schema_sizes function")
   gather_monthly_sizes = PostgresOperator(
        task_id='execute_schema_sizes_function',
        sql="'SELECT ods_data_management.ods_collect_schema_sizes();'",
@@ -38,10 +36,13 @@ with DAG(
        autocommit=True,
    )
  
-  logger.info("Executing collect_schema_stats function")
   gather_monthly_totals = PostgresOperator(
        task_id='execute_schema_totals_function',
        sql="'SELECT ods_data_management.ods_collect_schema_totals();'",
        postgres_conn_id="postgres_ods_conn",
-       autocommit=True,
-  ) 
+       autocommit=True, autocommit=True,
+    )    
+    
+  gather_monthly_sizes >> gather_monthly_totals
+
+exe_ods_collect_schema_stats()
