bf8026f1afff8063b7b7663677e0eb62c611446b
Jakia Sultana <jakia.sultana2318@yahoo.com>
2025-01-23 10:16:06 -0800

    Modified scripts to add on hold, net processing time etc

--- dags/include/ats_housing_expire_old_records.sql
+++ dags/include/ats_housing_expire_old_records.sql
@@ -57,13 +57,25 @@ select
 			coalesce(cast(a.housing_type					 		as varchar),'~') || '|'|| 
 			coalesce(cast(a.estimated_housing	    		 		as varchar),'~') || '|'|| 
 			coalesce(cast(a.application_status			 		as varchar),'~') || '|'|| 
-			coalesce(cast(a.business_area_file_number			 		as varchar),'~')
+			coalesce(cast(a.business_area_file_number		 		as varchar),'~') || '|'||
+            coalesce(cast(a.on_hold_reason_code					as varchar),'~') || '|'|| 
+            coalesce(cast(a.on_hold_reason_description	 		as varchar),'~') || '|'|| 
+            coalesce(cast(a.on_hold_start_date					as varchar),'~') || '|'|| 
+            coalesce(cast(a.on_hold_end_date						as varchar),'~') || '|'|| 
+            coalesce(cast(a.total_on_hold_time					as varchar),'~') || '|'|| 
+            coalesce(cast(a.net_processing_time					as varchar),'~')        
 			) as key_hash,
 		'A' as record_active_ind,
 		current_timestamp(0)			effective_start_dttm, 
 		'9999-12-31 00:00:00'		effective_end_dttm,
 		a.record_created_by record_created_by,
-		a.record_created_dttm record_created_dttm		
+		a.record_created_dttm record_created_dttm,
+		a.on_hold_reason_code,
+		a.on_hold_reason_description,
+		a.on_hold_start_date,
+		a.on_hold_end_date,
+		a.total_on_hold_time,
+		a.net_processing_time			
 FROM	lob_dq_replication.ats_housing  a
 left 	join  ( select * from lob_dq_replication_hist.ats_housing_hist where effective_end_dttm=CAST('9999-12-31 00:00:00' as timestamp(0)))d on 
         a.application_id = d.application_id and
@@ -94,7 +106,13 @@ left 	join  ( select * from lob_dq_replication_hist.ats_housing_hist where effec
 			coalesce(cast(a.housing_type					 		as varchar),'~') || '|'|| 
 			coalesce(cast(a.estimated_housing	    		 		as varchar),'~') || '|'|| 
 			coalesce(cast(a.application_status			 		as varchar),'~') || '|'|| 
-			coalesce(cast(a.business_area_file_number			 		as varchar),'~')
+			coalesce(cast(a.business_area_file_number		 		as varchar),'~') || '|'||
+            coalesce(cast(a.on_hold_reason_code					as varchar),'~') || '|'|| 
+            coalesce(cast(a.on_hold_reason_description	 		as varchar),'~') || '|'|| 
+            coalesce(cast(a.on_hold_start_date					as varchar),'~') || '|'|| 
+            coalesce(cast(a.on_hold_end_date						as varchar),'~') || '|'||
+            coalesce(cast(a.total_on_hold_time					as varchar),'~') || '|'|| 
+            coalesce(cast(a.net_processing_time					as varchar),'~')  
 			)=d.hash_key 	 
  where d.hash_key is null and 
  a.application_id in (select application_id from lob_dq_replication_hist.ats_housing_hist where effective_end_dttm=CAST('9999-12-31 00:00:00' as timestamp(0)))
