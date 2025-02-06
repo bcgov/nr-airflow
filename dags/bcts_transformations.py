407dae97b6cb08b57c1f91b71e85a8bf37b7df18
Sreejith Munthikodu <sreejimunthikodu@yahoo.com>
2025-02-05 16:16:48 -0800

    Added dags for BCTS Performance Report

--- dags/bcts_transformations.py
+++ dags/bcts_transformations.py
@@ -43,13 +43,31 @@ with DAG(
     description='DAG to run the transformations in ODS for BCTS Annual Developed Volume Dashboard',
 ) as dag:
     
-    wait_for_replication = ExternalTaskSensor(
-        task_id='wait_for_replication',
+    wait_for_lrm_replication = ExternalTaskSensor(
+        task_id='wait_for_lrm_replication',
         external_dag_id='bcts-replication-lrm',
         external_task_id='task_completion_flag',
         timeout=60000,  # Timeout in seconds
         poke_interval=30,  # How often to check (in seconds)
         execution_delta = timedelta(minutes=15)
+    )
+
+    wait_for_bctsadmin_replication = ExternalTaskSensor(
+        task_id='wait_for_bctsadmin_replication',
+        external_dag_id='bcts-replication-bctsadmin',
+        external_task_id='task_completion_flag',
+        timeout=60000,  # Timeout in seconds
+        poke_interval=30,  # How often to check (in seconds)
+        execution_delta = timedelta(minutes=40)
+    )
+
+    wait_for_bcts_client_replication = ExternalTaskSensor(
+        task_id='wait_for_bcts_client_replication',
+        external_dag_id='bcts-replication-client',
+        external_task_id='task_completion_flag',
+        timeout=60000,  # Timeout in seconds
+        poke_interval=30,  # How often to check (in seconds)
+        execution_delta = timedelta(minutes=35)
     )
     
     if ENV == 'LOCAL':
@@ -91,8 +109,32 @@ with DAG(
             random_name_suffix=False
         )
 
+
+        bcts_performance_report_transformation = KubernetesPodOperator(
+            task_id="bcts_performance_report_transformation",
+            image="ghcr.io/bcgov/nr-dap-ods-bctstransformations:SD-132828-ODS-MIGRATION-BCTS-PERFORMANCE-REPORT",
+            cmds=["python3", "./bcts_performance_report_transformation.py"],
+            image_pull_policy="Always",
+            in_cluster=True,
+            service_account_name="airflow-admin",
+            name=f"run_{LOB}_transformation_annual_developed_volume",
+            labels={"DataClass": "Medium", "ConnectionType": "database",  "Release": "airflow"},
+            is_delete_operator_pod=True,
+            secrets=[ods_secrets],
+            container_resources= client.V1ResourceRequirements(
+            requests={"cpu": "50m", "memory": "512Mi"},
+            limits={"cpu": "100m", "memory": "1024Mi"}),
+            random_name_suffix=False
+        )
+
     task_completion_flag = DummyOperator(
         task_id='task_completion_flag'
     )
 
-    wait_for_replication >> annual_developed_volume_transformation >> task_completion_flag
+    wait_for_lrm_replication >> annual_developed_volume_transformation >> task_completion_flag
+    # Tasks within the same DAG should run in sequence because the pod names are forced to be static to whitelist in DBP06. Running in parallel can cause error due to pod name conflict
+    annual_developed_volume_transformation >> bcts_performance_report_transformation
+    wait_for_lrm_replication >> bcts_performance_report_transformation
+    wait_for_bctsadmin_replication >> bcts_performance_report_transformation
+    wait_for_bcts_client_replication >> bcts_performance_report_transformation
+    bcts_performance_report_transformation >> task_completion_flag
