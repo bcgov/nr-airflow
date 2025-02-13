7e6572a399e15e401a8ebf4acd50149b400af999
Sreejith Munthikodu <sreejimunthikodu@yahoo.com>
2025-02-13 09:28:55 -0800

    Added timber sales inventory ready to sell report transformation

--- dags/bcts_transformations.py
+++ dags/bcts_transformations.py
@@ -117,7 +117,24 @@ with DAG(
             image_pull_policy="Always",
             in_cluster=True,
             service_account_name="airflow-admin",
-            name=f"run_{LOB}_transformation_annual_developed_volume",
+            name=f"run_{LOB}_transformation_bcts_performance_report",
+            labels={"DataClass": "Medium", "ConnectionType": "database",  "Release": "airflow"},
+            is_delete_operator_pod=True,
+            secrets=[ods_secrets],
+            container_resources= client.V1ResourceRequirements(
+            requests={"cpu": "50m", "memory": "512Mi"},
+            limits={"cpu": "100m", "memory": "1024Mi"}),
+            random_name_suffix=False
+        )
+
+        bcts_timber_inventory_ready_to_sell_report_transformation = KubernetesPodOperator(
+            task_id="bcts_timber_inventory_ready_to_sell_report_transformation",
+            image="ghcr.io/bcgov/nr-dap-ods-bctstransformations:SD-137267-Timber-Inventory-Ready-to-Sell-Report",
+            cmds=["python3", "./bcts_timber_inventory_ready_to_sell_transformation.py"],
+            image_pull_policy="Always",
+            in_cluster=True,
+            service_account_name="airflow-admin",
+            name=f"run_{LOB}_timber_inventory_ready_to_sell",
             labels={"DataClass": "Medium", "ConnectionType": "database",  "Release": "airflow"},
             is_delete_operator_pod=True,
             secrets=[ods_secrets],
@@ -131,10 +148,15 @@ with DAG(
         task_id='task_completion_flag'
     )
 
-    wait_for_lrm_replication >> annual_developed_volume_transformation >> task_completion_flag
-    # Tasks within the same DAG should run in sequence because the pod names are forced to be static to whitelist in DBP06. Running in parallel can cause error due to pod name conflict
-    annual_developed_volume_transformation >> bcts_performance_report_transformation
+    wait_for_lrm_replication >> annual_developed_volume_transformation 
     wait_for_lrm_replication >> bcts_performance_report_transformation
+    wait_for_lrm_replication >> bcts_timber_inventory_ready_to_sell_report_transformation
     wait_for_bctsadmin_replication >> bcts_performance_report_transformation
     wait_for_bcts_client_replication >> bcts_performance_report_transformation
+    
+    annual_developed_volume_transformation >> task_completion_flag
     bcts_performance_report_transformation >> task_completion_flag
+    bcts_timber_inventory_ready_to_sell_report_transformation >> task_completion_flag
+
+
+    
