14a159c22681540711c35f749157d91e9fd2146f
Munthikodu <sreejith.munthikodu@gov.bc.ca>
2025-04-23 16:18:00 -0700

    Added BCTS Admin and FOREST Client replications

--- dags/bcts_lrm_forest_replication.py
+++ dags/bcts_lrm_forest_replication.py
@@ -64,7 +64,7 @@ with DAG(
         # In Dev, Test, and Prod Environments
         run_replication = KubernetesPodOperator(
             task_id="run_replication",
-            image="ghcr.io/bcgov/nr-dap-ods-ora2pg_bcts:Ora2pg-FASTER-LOAD-TO-POSTGRES",
+            image="ghcr.io/bcgov/nr-dap-ods-ora2pg_bcts:main",
             image_pull_policy="Always",
             in_cluster=True,
             service_account_name="airflow-admin",
