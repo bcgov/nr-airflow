193d598e502182e66be47f4d0cae662a09681634
Munthikodu <sreejith.munthikodu@gov.bc.ca>
2025-04-30 13:25:42 -0700

    added bcts roads_transferred in out transformations

--- dags/bcts_apply_grants.py
+++ dags/bcts_apply_grants.py
@@ -71,7 +71,7 @@ with DAG(
         # In Dev, Test, and Prod Environments
         run_replication = KubernetesPodOperator(
             task_id=f"apply_bcts_grants",
-            image="ghcr.io/bcgov/nr-dap-ods-bctsgrantmngmt:main",
+            image="ghcr.io/bcgov/nr-dap-ods-bctsgrantmngmt:SD-145225-BCTS-ROADS-TRANSFERRED-IN-OUT",
             cmds=["python3", "./bcts_acces_apply_grants.py"],
             image_pull_policy="Always",
             in_cluster=True,
--- dags/bcts_export_grants_master_file.py
+++ dags/bcts_export_grants_master_file.py
@@ -62,7 +62,7 @@ with DAG(
         # In Dev, Test, and Prod Environments
         run_replication = KubernetesPodOperator(
             task_id=f"export_bcts_grants",
-            image="ghcr.io/bcgov/nr-dap-ods-bctsgrantmngmt:main",
+            image="ghcr.io/bcgov/nr-dap-ods-bctsgrantmngmt:SD-145225-BCTS-ROADS-TRANSFERRED-IN-OUT",
             cmds=["python3", "./bcts_access_export_master_file.py"],
             image_pull_policy="Always",
             in_cluster=True,
