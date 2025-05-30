677a5e6e2d75dbe41319d02b1da69ed84e7b55a1
Munthikodu <sreejith.munthikodu@gov.bc.ca>
2025-05-30 09:35:20 -0700

    Added dags for BCTS performance report

--- dags/bcts_transformations.py
+++ dags/bcts_transformations.py
@@ -50,6 +50,33 @@ with DAG(
         poke_interval=30,  # How often to check (in seconds)
         execution_delta = timedelta(minutes=15)
     )
+
+    wait_for_bctsadmin_replication = ExternalTaskSensor(
+        task_id='wait_for_bctsadmin_replication',
+        external_dag_id='bcts-replication-bctsadmin',
+        external_task_id='task_completion_flag',
+        timeout=60000,  # Timeout in seconds
+        poke_interval=30,  # How often to check (in seconds)
+        execution_delta = timedelta(minutes=40)
+    )
+
+    wait_for_bcts_client_replication = ExternalTaskSensor(
+        task_id='wait_for_bcts_client_replication',
+        external_dag_id='bcts-replication-client',
+        external_task_id='task_completion_flag',
+        timeout=60000,  # Timeout in seconds
+        poke_interval=30,  # How often to check (in seconds)
+        execution_delta = timedelta(minutes=35)
+    )
+
+    wait_for_fta_data_import = ExternalTaskSensor(
+    task_id='wait_for_fta_data_import',
+    external_dag_id='bcts_import_fta_data',
+    external_task_id='bcts_import_fta_data',
+    timeout=60000,  # Timeout in seconds
+    poke_interval=30,  # How often to check (in seconds)
+    execution_delta = timedelta(minutes=10)
+    )
     
     # In Dev, Test, and Prod Environments
     bcts_annual_developed_volume_transformation = KubernetesPodOperator(
@@ -122,6 +149,23 @@ with DAG(
         random_name_suffix=False
 
     )
+
+    bcts_performance_report_transformation = KubernetesPodOperator(
+            task_id="bcts_performance_report_transformation",
+            image="ghcr.io/bcgov/nr-dap-ods-bctstransformations:main",
+            cmds=["python3", "./bcts_performance_report_transformation.py"],
+            image_pull_policy="Always",
+            in_cluster=True,
+            service_account_name="airflow-admin",
+            name=f"run_{LOB}_transformation_bcts_performance_report",
+            labels={"DataClass": "Medium", "ConnectionType": "database",  "Release": "airflow"},
+            is_delete_operator_pod=True,
+            secrets=[ods_secrets],
+            container_resources= client.V1ResourceRequirements(
+            requests={"cpu": "50m", "memory": "512Mi"},
+            limits={"cpu": "100m", "memory": "1024Mi"}),
+            random_name_suffix=False
+        )
 
     bcts_roads_transferred_in_report_transformation = KubernetesPodOperator(
             task_id="bcts_roads_transferred_in_report_transformation",
@@ -235,16 +279,21 @@ with DAG(
     wait_for_lrm_replication >> bcts_timber_inventory_ready_to_develop_report_transformation
     wait_for_lrm_replication >> bcts_annual_development_ready_report_transformation
     wait_for_lrm_replication >> bcts_publish_forestview_views
+    wait_for_lrm_replication >> bcts_performance_report_transformation
     wait_for_lrm_replication >> bcts_roads_transferred_in_report_transformation
     wait_for_lrm_replication >> bcts_roads_transferred_out_report_transformation
     wait_for_lrm_replication >> bcts_roads_constructed_report_transformation
     wait_for_lrm_replication >> bcts_roads_deactivated_report_transformation
     wait_for_lrm_replication >> bcts_timber_inventory_development_in_progress_report_transformation
+    wait_for_bctsadmin_replication >> bcts_performance_report_transformation
+    wait_for_bcts_client_replication >> bcts_performance_report_transformation
+    wait_for_fta_data_import >> bcts_performance_report_transformation
     
     bcts_annual_developed_volume_transformation >> task_completion_flag
     bcts_timber_inventory_ready_to_sell_report_transformation >> task_completion_flag
     bcts_timber_inventory_ready_to_develop_report_transformation >> task_completion_flag
     bcts_annual_development_ready_report_transformation >> task_completion_flag
+    bcts_performance_report_transformation >> task_completion_flag
     bcts_publish_forestview_views >> task_completion_flag
     bcts_roads_transferred_in_report_transformation >> task_completion_flag
     bcts_roads_transferred_out_report_transformation >> task_completion_flag
