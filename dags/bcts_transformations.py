df547e7aa073ddd968b3f901ca1f731026122b21
Munthikodu <sreejith.munthikodu@gov.bc.ca>
2025-08-05 10:34:30 -0700

    Removed stale bcts dag and updated schedule

--- dags/bcts_transformations.py
+++ dags/bcts_transformations.py
@@ -36,7 +36,7 @@ else:
 with DAG(
     start_date=datetime(2024, 10, 23),
     catchup=False,
-    schedule='45 12 * * MON-FRI',
+    schedule='45 12 * * *',
     dag_id=f"bcts_transformations",
     default_args=default_args,
     description='DAG to run the transformations in ODS for BCTS Annual Developed Volume Dashboard',
