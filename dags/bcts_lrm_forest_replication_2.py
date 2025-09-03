a4c248e2a3d38dd01780ce83d8cbfb8a22a34deb
Munthikodu <sreejith.munthikodu@gov.bc.ca>
2025-09-03 11:01:43 -0700

    BCTS silviculture liability report

--- dags/bcts_lrm_forest_replication_2.py
+++ dags/bcts_lrm_forest_replication_2.py
@@ -13,7 +13,9 @@ LOB = 'lrm2'
 # For local development environment only.
 ENV = os.getenv("AIRFLOW_ENV")
 
-ods_secrets = Secret("env", None, f"{LOB}-ods-database")
+# Use same ODS secrets as existing LRM replication DAG
+ods_secrets = Secret("env", None, f"lrm-ods-database")
+# Use different LOB secrets for this second LRM replication DAG as app_name = lrm2 to grab list of tables to replicate from cdc_master_table_list.
 lob_secrets = Secret("env", None, f"{LOB}-database")
 
 if ENV == 'LOCAL':
