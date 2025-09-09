6b565c9c7b032060f96939eeaf5c503e53ac61cc
Munthikodu <sreejith.munthikodu@gov.bc.ca>
2025-09-09 16:38:03 -0700

    Added silviculture liability report transformation

---
+++ dags/bcts_lrm_forest_replication_2.py
@@ -0,0 +1,89 @@
+# Second DAG for LRM replication to ODS for BCTS Annual Developed Volume Dashboard
+# Existing DAG "bcts-replication-lrm" is causing OOM errors on additional tables due to large data volume.
+from airflow import DAG
+from pendulum import datetime
+from kubernetes import client
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+from airflow.providers.cncf.kubernetes.secret import Secret
+from airflow.operators.dummy import DummyOperator
+from datetime import timedelta
+import os
+
+LOB = 'lrm2'
+# For local development environment only.
+ENV = os.getenv("AIRFLOW_ENV")
+
+# Use same ODS secrets as existing LRM replication DAG
+ods_secrets = Secret("env", None, f"{LOB}-ods-database")
+# Use different LOB secrets for this second LRM replication DAG as app_name = lrm2 to grab list of tables to replicate from cdc_master_table_list.
+lob_secrets = Secret("env", None, f"{LOB}-database")
+
+if ENV == 'LOCAL':
+    default_args = {
+        'owner': 'BCTS',
+        "email": ["NRM.DataFoundations@gov.bc.ca"],
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),
+        "email_on_failure": False, # No alerts in local environment
+        "email_on_retry": False,
+    }
+else:
+    default_args = {
+        'owner': 'BCTS',
+        "email": ["NRM.DataFoundations@gov.bc.ca"],
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),
+        "email_on_failure": True,
+        "email_on_retry": False,
+    }
+
+with DAG(
+    start_date=datetime(2024, 10, 23),
+    catchup=False,
+    schedule='42 12 * * *',   # 4:42 AM PST
+    dag_id=f"bcts-replication-lrm2",
+    default_args=default_args,
+    description='DAG to replicate second batch of LRM data to ODS for BCTS Annual Developed Volume Dashboard',
+) as dag:
+    
+    if ENV == 'LOCAL':
+
+        run_replication = KubernetesPodOperator(
+            task_id="run_replication",
+            image="nrids-bcts-data-ora2pg:main",
+            # Following configs are different in the local development environment
+            # image_pull_policy="Always",
+            # in_cluster=True,
+            # service_account_name="airflow-admin",
+            name=f"run_{LOB}_replication",
+            labels={"DataClass": "Medium", "ConnectionType": "database",  "Release": "airflow"},
+            is_delete_operator_pod=True,
+            secrets=[lob_secrets, ods_secrets],
+            container_resources= client.V1ResourceRequirements(
+            requests={"cpu": "50m", "memory": "512Mi"},
+            limits={"cpu": "100m", "memory": "1024Mi"})
+        )
+        
+    else:
+        # In Dev, Test, and Prod Environments
+        run_replication = KubernetesPodOperator(
+            task_id="run_replication",
+            image="ghcr.io/bcgov/nr-dap-ods-ora2pg_bcts:main",
+            image_pull_policy="Always",
+            in_cluster=True,
+            service_account_name="airflow-admin",
+            name=f"run_{LOB}_replication",
+            labels={"DataClass": "Medium", "ConnectionType": "database",  "Release": "airflow"},
+            is_delete_operator_pod=True,
+            secrets=[lob_secrets, ods_secrets],
+            container_resources= client.V1ResourceRequirements(
+            requests = {"cpu": "400m", "memory": "4096Mi"},
+            limits = {"cpu": "800m", "memory": "6144Mi"}),
+            random_name_suffix=False
+        )
+
+    task_completion_flag = DummyOperator(
+        task_id='task_completion_flag'
+    )
+
+    run_replication >> task_completion_flag
---
+++ dags/bcts_results_replication.py
@@ -0,0 +1,57 @@
+from airflow import DAG
+from pendulum import datetime
+from kubernetes import client
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+from airflow.providers.cncf.kubernetes.secret import Secret
+from airflow.operators.dummy import DummyOperator
+from datetime import timedelta
+import os
+
+LOB = 'results'
+# For local development environment only.
+ENV = os.getenv("AIRFLOW_ENV")
+
+ods_secrets = Secret("env", None, f"lrm-ods-database")
+lob_secrets = Secret("env", None, f"results-database")
+
+
+default_args = {
+    'owner': 'BCTS',
+    "email": ["NRM.DataFoundations@gov.bc.ca"],
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    "email_on_failure": True,
+    "email_on_retry": False,
+}
+
+with DAG(
+    start_date=datetime(2024, 10, 23),
+    catchup=False,
+    schedule='40 12 * * *', # 4:40 AM PST
+    dag_id=f"bcts-replication-results",
+    default_args=default_args,
+    description='DAG to replicate RESULTS data to ODS for BCTS Reporting',
+) as dag:
+    
+    run_replication = KubernetesPodOperator(
+        task_id="run_replication",
+        image="ghcr.io/bcgov/nr-dap-ods-ora2pg_bcts:main",
+        image_pull_policy="Always",
+        in_cluster=True,
+        service_account_name="airflow-admin",
+        name=f"run_{LOB}_replication",
+        labels={"DataClass": "Medium", "ConnectionType": "database",  "Release": "airflow"},
+        is_delete_operator_pod=True,
+        secrets=[lob_secrets, ods_secrets],
+        container_resources= client.V1ResourceRequirements(
+        requests = {"cpu": "100m", "memory": "1024Mi"},
+        limits   = {"cpu": "200m", "memory": "2048Mi"}
+        ),
+        random_name_suffix=False
+    )
+
+    task_completion_flag = DummyOperator(
+        task_id='task_completion_flag'
+    )
+
+    run_replication >> task_completion_flag
